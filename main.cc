632d12b402896baebfa1f5fec83670e2c9a49da2
Marc Kirchner <mail@marc-kirchner.de>
2019-01-12 17:25:50 -0800

    First version of diffuse materials and recursion

--- main.cc
+++ main.cc
@@ -19,11 +19,23 @@
 const Vec3 WHITE = Vec3(1.0, 1.0, 1.0);
 const Vec3 BLUE = Vec3(0.2, 0.5, 1.0);
 
-Vec3 color(const Ray& r, const ObjCollection& world) {
+Vec3 sample_from_unit_sphere() {
+    Vec3 p;
+    do {
+        p = 2.0 * Vec3(drand48(), drand48(), drand48()) - Vec3(1.0, 1.0, 1.0);
+    } while (p.squared_norm() >= 1.0);
+    return p;
+}
+
+Vec3 color(const Ray& r, const ObjCollection& world, int n) {
     Hit hit;
-    if (world.getHit(r, 0.0, FLT_MAX, hit)) {
-        // std::cout << "Hit" << std::endl;
-        return 0.5*Vec3(hit.n.x()+1, hit.n.y()+1, hit.n.z()+1);
+    if (world.getHit(r, 0.001, FLT_MAX, hit)) {
+        Vec3 reflection = hit.p + hit.n + sample_from_unit_sphere();
+        if (n > 10) {
+          return 0.5*Vec3(hit.n.x()+1, hit.n.y()+1, hit.n.z()+1);
+        } else {
+            return 0.5*color(Ray(hit.p, reflection-hit.p), world, n+1);
+        }
     } else {
         float t = 0.5 * (r.direction().normalized().y() + 1.0);
         Vec3 lerp = (1.0-t) * WHITE + t * BLUE;
@@ -58,9 +70,12 @@ void writeImage() {
                 float u = float(x + drand48())/float(nx);
                 float v = float(y + drand48())/float(ny);
                 Ray r = cam.getRay(u, v); 
-                px += color(r, world);
+                px += color(r, world, 0);
             }
-            px = px / ns * 255.99;
+            px = px / ns;
+            // approximate gamma correction
+            px = Vec3(sqrt(px[0]), sqrt(px[1]), sqrt(px[2]));
+            px *= 255.99;
             std::cout << int(px[0]) << " " << int(px[1]) << " "
                       << int(px[2]) << "\n";
         }
